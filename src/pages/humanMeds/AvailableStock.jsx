2e497b8d56703efc09637ed8a40c4a8c785b20f1
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-09-21 11:46:14 +0100

    Make search bar effective

--- src/pages/humanMeds/AvailableStock.jsx
+++ src/pages/humanMeds/AvailableStock.jsx
@@ -1,36 +1,12 @@
 import { useState } from "react";
+import { useContext } from "react";
 import { useEffect } from "react";
 import { GrEdit } from "react-icons/gr";
 import { ClipLoader } from "react-spinners";
+import { HumanWrapContext } from "./HumanWrap";
 
 const AvailStock = () => {
-  const tosinToken = localStorage.getItem('user');
-  const token = JSON.parse(tosinToken);
-
-  const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect( () => {
-    setIsLoading(true);
-    const fetchData = async () => {
-      try {
-        const res = await fetch('https://nvmri.onrender.com/user/all/drugs', {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${token.token}`,
-          },
-        });
-        const data = await res.json();
-        setData(data);
-        setIsLoading(false);
-      } catch (err) {
-        console.log(err);
-        setIsLoading(false);
-      }
-    }
-    fetchData();
-  }, [])
+  const { isLoading, filteredData } = useContext(HumanWrapContext)
 
   return ( 
     <>
@@ -46,23 +22,29 @@ const AvailStock = () => {
           <div className="loaded">
             <ClipLoader color="#007bff" className="loadImg" loading={isLoading} size={60}/>
           </div>
-        ) : (<>
-          {data && data.map ((drug) => (
-            <div className="stockDetails" key={drug._id}>
-              <p>{drug.name}</p>
-              <p>{drug.quantity_type}</p>
-              <p>
-                {drug.quantity_type==='carton' ? drug.carton : drug.quantity_type==='pack' ? drug.pack : drug.quantity_type==='card' ? drug.card : drug.tablet}
-              </p>
-              <p>₦ {drug.price}</p>
-              <div className="date">
-                <p>{drug.expiryDate}</p>
-                <GrEdit/>
+        ) : (
+          <>
+            {filteredData.length > 0 ? (
+              filteredData.map ((drug) => (
+              <div className="stockDetails" key={drug._id}>
+                <p>{drug.name}</p>
+                <p>{drug.quantity_type}</p>
+                <p>
+                  {drug.quantity_type==='carton' ? drug.carton : drug.quantity_type==='pack' ? drug.pack : drug.quantity_type==='card' ? drug.card : drug.tablet}
+                </p>
+                <p>₦ {drug.price}</p>
+                <div className="date">
+                  <p>{drug.expiryDate}</p>
+                  <GrEdit/>
+                </div>
               </div>
-            </div>
+              )
+            )) : (
+              <p className="notFound">Drug not found</p>
             )
-          )}
-        </>)}
+            }
+          </>
+        )}
       </div>
     </>
   );
--- src/pages/humanMeds/HumanWrap.jsx
+++ src/pages/humanMeds/HumanWrap.jsx
@@ -4,12 +4,57 @@ import { Outlet, NavLink, useLocation, useNavigate } from 'react-router-dom';
 import search from '../../assets/icons/search.svg';
 import notification from '../../assets/icons/notification.svg';
 import Popup from '../Popup/Popup';
+import { useEffect, useState } from 'react';
+import { createContext } from 'react';
 
+const HumanWrapContext = createContext();
 const HumanWrap = () => {
   const location = useLocation();
   const history = useNavigate();
+  const [search, setSearch] = useState('');
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const tosinToken = localStorage.getItem('user');
+  const token = JSON.parse(tosinToken);
 
   const button = location.pathname.includes('/Expired') || location.pathname.includes('/Items-Sold');
+
+  useEffect( () => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const res = await fetch('https://nvmri.onrender.com/user/all/drugs', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token.token}`,
+          },
+        });
+        const data = await res.json();
+        setData(data);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+      }
+    }
+    fetchData();
+  }, [])
+
+  useEffect(() => {
+    const searchWords = search.toLowerCase().split(' ');
+    const filteredStock = data.filter((stock) => {
+      const drugName = `${stock.name}`.toLowerCase();
+      return searchWords.every((word) => drugName.includes(word));
+    });
+    setFilteredData(filteredStock);
+  }, [search, data]);
+
+  const humanWrapContextValue = {
+    isLoading, filteredData
+  }
 
   return ( 
     <>
@@ -22,7 +67,12 @@ const HumanWrap = () => {
           <div className="bottomHeader tree">
             <div className="searchForm">
               <img src={search} alt="" />
-              <input placeholder='Search' type="text" />
+              <input 
+                placeholder='Search' 
+                type="text" 
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
             </div>
             {!button && (
               <Popup/>
@@ -36,10 +86,15 @@ const HumanWrap = () => {
         <NavLink to='Expired'>Expired</NavLink>
       </nav>
       <main className="wrapper">
-        <Outlet/>
+        <HumanWrapContext.Provider value={humanWrapContextValue}>
+          <Outlet/>
+        </HumanWrapContext.Provider>
       </main>
     </>
   );
+
+  
 }
  
 export default HumanWrap;
+export { HumanWrapContext };
