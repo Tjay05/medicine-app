1a1b936010c80a3d36338ab7a391512f6020b7d1
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-10-02 16:15:19 +0100

    Add styles for printed pdf

--- src/pages/MyReceipt.jsx
+++ src/pages/MyReceipt.jsx
@@ -14,7 +14,7 @@ const MyDocument = () => {
   const componentRef = useRef();
 
   return (
-   <div ref={componentRef} className="recieptView">
+   <div ref={componentRef} className="recieptView" style={{display: 'flex', flexDirection: 'column'}}>
     <header>
       <h1>{shopName}</h1>
       <p>Address: {address}</p>
@@ -23,35 +23,37 @@ const MyDocument = () => {
     </header>
     <h2>RECEIPT</h2>
     <section>
-      <table style={{width: '100%'}}>
-        <tbody>
-          <tr>
-            <th>DESCRIPTION</th>
-            <th>QUANTITY</th>
-            <th>PRICE</th>
-          </tr>
-          <tr>
-            <td>Nise Syrup</td>
-            <td>2</td>
-            <td>₦ 1000</td>
-          </tr>
-          <tr>
-            <td>Nise Syrup</td>
-            <td>2</td>
-            <td>₦ 1000</td>
-          </tr>
-          <div className="tableFooter">
+      <div className="topTable">
+        <table style={{width: '100%'}}>
+          <tbody>
             <tr>
-              <td>TOTAL</td>
-              <td>₦ 2000</td>
+              <th>DESCRIPTION</th>
+              <th>QUANTITY</th>
+              <th>PRICE</th>
             </tr>
-          </div>
-        </tbody>
-      </table>
+            <tr>
+              <td>Nise Syrup</td>
+              <td>2</td>
+              <td>₦ 1000</td>
+            </tr>
+            <tr>
+              <td>Nise Syrup</td>
+              <td>2</td>
+              <td>₦ 1000</td>
+            </tr>
+            <div className="tableFooter">
+              <tr>
+                <td>TOTAL</td>
+                <td>₦ 2000</td>
+              </tr>
+            </div>
+          </tbody>
+        </table>
+      </div>
     </section>
     <h3>THANK YOU!</h3>
     <ReactToPrint 
-      trigger={() => <button>Print me</button>}
+      trigger={() => <button style={{backgroundColor: '#003366', color: 'white', border: 'none', padding: '.5rem 1rem', borderRadius: '5px', marginBlockStart: '.8rem'}}>Print me</button>}
       content={() => componentRef.current}
     />
    </div>
