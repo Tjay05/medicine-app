5a9cd19b734b0e2df53841963805c6fa5dbc9e78
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-10-31 19:05:59 +0100

    Edit style for color

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import Navbar from "./layout/Navbar";
 import Login from "./pages/Login";
 
-import { 
+import {
   createBrowserRouter,
   Route,
   createRoutesFromElements,
-  RouterProvider
+  RouterProvider,
 } from "react-router-dom";
 
 // Admin Pages
@@ -31,38 +31,38 @@ import CashNotifications from "./pages/cashier/Notifications";
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route index element={<Login/>} />
-      <Route element={<Navbar/>}>
-        <Route path="Home" element={<Dashboard/>} />
-        <Route element={<HumanWrap/>}>
-          <Route path="Human-Medicine" element={<AvailStock/>} />
-          <Route path="Items-Sold" element={<ItemSold/>} />
-          <Route path="Expired" element={<Expired/>} />
+      <Route index element={<Login />} />
+      <Route element={<Navbar />}>
+        <Route path="Home" element={<Dashboard />} />
+        <Route element={<HumanWrap />}>
+          <Route path="Human-Medicine" element={<AvailStock />} />
+          <Route path="Items-Sold" element={<ItemSold />} />
+          <Route path="Expired" element={<Expired />} />
         </Route>
-        <Route  element={<AnimalWrap/>} >
-          <Route path="Animal-Medicine" element={<AvailableStock/>} />
-          <Route path="Items-Sold" element={<ItemsSold/>} />
-          <Route path="Expired" element={<ExpiredMeds/>} />
+        <Route element={<AnimalWrap />}>
+          <Route path="Animal-Medicine" element={<AvailableStock />} />
+          <Route path="Items-Sold" element={<ItemsSold />} />
+          <Route path="Expired" element={<ExpiredMeds />} />
         </Route>
-        <Route path="Employee" element={<Employee/>} />
-        <Route path="Receipt" element={<Receipt/>} />
-        <Route path="Notifications" element={<Notification/>} />
+        <Route path="Employee" element={<Employee />} />
+        <Route path="Receipt" element={<Receipt />} />
+        <Route path="Notifications" element={<Notification />} />
       </Route>
-      <Route element={<CashierNavbar/>}>
-        <Route path="Sales" element={<Sales/>} />
-        <Route path="Cashier-Receipts" element={<CashReciept/>} />
-        <Route path="Cashier-Notification" element={<CashNotifications/>} />
+      <Route element={<CashierNavbar />}>
+        <Route path="Sales" element={<Sales />} />
+        <Route path="Cashier-Receipts" element={<CashReciept />} />
+        <Route path="Cashier-Notification" element={<CashNotifications />} />
       </Route>
     </Route>
   )
-)
+);
 
 function App() {
   return (
     <>
       <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
