d1755d4a31096fc363cf8190d17acfed65d268d4
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-11-03 20:26:44 +0100

    format document

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,14 @@
 // Picture Imports
-import medCare from '../assets/images/undraw_medical_care_movn.png'
-import logo from '../assets/icons/logo.svg'
-import { useNavigate } from 'react-router-dom';
-import { useEffect, useState } from 'react';
-import { useJwt } from 'react-jwt';
+import medCare from "../assets/images/undraw_medical_care_movn.png";
+import logo from "../assets/icons/logo.svg";
+import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useJwt } from "react-jwt";
 import { RingLoader } from "react-spinners";
 
 const Login = () => {
-  const [id, setId] = useState('');
-  const [data, setData] = useState('');
+  const [id, setId] = useState("");
+  const [data, setData] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
   const history = useNavigate();
@@ -17,39 +17,38 @@ const Login = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch('https://nvmri.onrender.com/user/login', {
-        method: 'POST',
+      const response = await fetch("https://nvmri.onrender.com/user/login", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          id
+          id,
         }),
       });
-      const data = await response.json()
+      const data = await response.json();
       setData(data);
       if (response.ok) {
         setData(data);
         tokenValidation();
-        setIsLoading(false)
-      } else if(response.status === 401) {
-          setData(data);
-          setIsLoading(false)
-        }else if (response.status === 404){
-          setData(data);
-          setIsLoading(false)
-        }else{
-          setData('Could not login user');
-          setIsLoading(false)
-        }
-    }
-    catch (error) {
+        setIsLoading(false);
+      } else if (response.status === 401) {
+        setData(data);
+        setIsLoading(false);
+      } else if (response.status === 404) {
+        setData(data);
+        setIsLoading(false);
+      } else {
+        setData("Could not login user");
+        setIsLoading(false);
+      }
+    } catch (error) {
       console.log("Topins' Error:", error);
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
-  const {decodedToken} = useJwt(data);
+  const { decodedToken } = useJwt(data);
 
   useEffect(() => {
     if (decodedToken) {
@@ -59,25 +58,34 @@ const Login = () => {
 
   const tokenValidation = () => {
     console.log(decodedToken);
-    if (decodedToken.role === 'admin') {
-      history('/Home');
-      localStorage.setItem('user', JSON.stringify({
-        token: data
-      }))
+    if (decodedToken.role === "admin") {
+      history("/Home");
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          token: data,
+        })
+      );
     } else {
-      history('/Sales')
-      localStorage.setItem('cashier', JSON.stringify({
-        token: data
-      }))
+      history("/Sales");
+      localStorage.setItem(
+        "cashier",
+        JSON.stringify({
+          token: data,
+        })
+      );
     }
-  }
+  };
 
-  return ( 
+  return (
     <>
-      <main className='loginMain'>
+      <main className="loginMain">
         <aside>
           <img src={medCare} alt="Effortless" />
-          <p>Effortlessly manage medicine inventory - track, organize, and stay informed</p>
+          <p>
+            Effortlessly manage medicine inventory - track, organize, and stay
+            informed
+          </p>
         </aside>
         <section>
           <nav>
@@ -88,21 +96,38 @@ const Login = () => {
             <h1>Log in</h1>
             <p>Please input your credentials to continue using this app</p>
             <form>
-              <input 
+              <input
                 value={id}
                 onChange={(e) => setId(e.target.value)}
-                placeholder='ID No.' 
-                type="text" 
+                placeholder="ID No."
+                type="text"
               />
-              <p style={data === decodedToken ?{ display: 'none'} : {display: 'inline-block'}} className="err-mssg">{data}</p>
-              {!isLoading && <button onClick={handleLogin} type='submit'>Continue</button>}
-              {isLoading && <button style={{cursor: 'not-allowed'}} disabled><RingLoader color='white' size={27} loading={isLoading}/></button>}
+              <p
+                style={
+                  data === decodedToken
+                    ? { display: "none" }
+                    : { display: "inline-block" }
+                }
+                className="err-mssg"
+              >
+                {data}
+              </p>
+              {!isLoading && (
+                <button onClick={handleLogin} type="submit">
+                  Continue
+                </button>
+              )}
+              {isLoading && (
+                <button style={{ cursor: "not-allowed" }} disabled>
+                  <RingLoader color="white" size={27} loading={isLoading} />
+                </button>
+              )}
             </form>
           </div>
         </section>
       </main>
     </>
-   );
-}
- 
+  );
+};
+
 export default Login;
