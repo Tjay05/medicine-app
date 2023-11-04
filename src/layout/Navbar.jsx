b4c09ffb08349b9d17059d39e4ed6a75b999fc1d
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-11-04 21:48:49 +0100

    format document

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,69 +1,83 @@
 // Picture Imports
-import logo from '../assets/icons/logo.svg';
-import dashIcon from '../assets/icons/dashboard.svg';
-import humanMed from '../assets/icons/hmuanMeds.svg';
-import reciept from '../assets/icons/reciept.svg';
-import employee from '../assets/icons/employees.svg';
-import { Outlet, NavLink } from 'react-router-dom'
-import { createContext, useState } from 'react';
+import logo from "../assets/icons/logo.svg";
+import dashIcon from "../assets/icons/dashboard.svg";
+import humanMed from "../assets/icons/hmuanMeds.svg";
+import reciept from "../assets/icons/reciept.svg";
+import employee from "../assets/icons/employees.svg";
+import { Outlet, NavLink } from "react-router-dom";
+import { createContext, useState } from "react";
 
 const PopupContext = createContext();
 const Navbar = () => {
   const [showPopup, setShowPopup] = useState(false);
   const handleClick = () => setShowPopup(!showPopup);
   // Special
-  const [quantity_type, setQuantity_type] = useState('');
+  const [quantity_type, setQuantity_type] = useState("");
   const handleTypeChange = (e) => setQuantity_type(e.target.value);
 
-
   const popupContextValue = {
-    showPopup, setShowPopup,
-    quantity_type, setQuantity_type,
-    handleClick, handleTypeChange
-  }
+    showPopup,
+    setShowPopup,
+    quantity_type,
+    setQuantity_type,
+    handleClick,
+    handleTypeChange,
+  };
 
-  return ( 
+  return (
     <>
-      <main className='navbarContent'>
-        <aside className='nav'>
-          <nav className='container'>
+      <main className="navbarContent">
+        <aside className="nav">
+          <nav className="container">
             <ul>
-              <li><a>
-                <img src={logo} alt="Logo" />
-                <p>Medicine App</p>
-              </a></li>
-              <li><NavLink to='/Home'>
-                <img src={dashIcon} alt="Dashboard" />
-                <p>Dashboard</p>
-              </NavLink></li>
-              <li><NavLink to='Human-Medicine'>
-                <img src={humanMed} alt="Human Medicines" />
-                <p>Human Medicines</p>
-              </NavLink></li>
-              <li><NavLink to='Animal-Medicine'>
-                <img src={humanMed} alt="Animal Medicines" />
-                <p>Animal Medicines</p>
-              </NavLink></li>
-              <li><NavLink to='Receipt'>
-                <img src={reciept} alt="Receipt" />
-                <p>Receipt</p>
-              </NavLink></li>
-              <li><NavLink to='Employee'>
-                <img src={employee} alt="Employees" />
-                <p>Employees</p>
-              </NavLink></li>
+              <li>
+                <a>
+                  <img src={logo} alt="Logo" />
+                  <p>Medicine App</p>
+                </a>
+              </li>
+              <li>
+                <NavLink to="/Home">
+                  <img src={dashIcon} alt="Dashboard" />
+                  <p>Dashboard</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Human-Medicine">
+                  <img src={humanMed} alt="Human Medicines" />
+                  <p>Human Medicines</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Animal-Medicine">
+                  <img src={humanMed} alt="Animal Medicines" />
+                  <p>Animal Medicines</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Receipt">
+                  <img src={reciept} alt="Receipt" />
+                  <p>Receipt</p>
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="Employee">
+                  <img src={employee} alt="Employees" />
+                  <p>Employees</p>
+                </NavLink>
+              </li>
             </ul>
           </nav>
         </aside>
-        <section className='content'>
+        <section className="content">
           <PopupContext.Provider value={popupContextValue}>
-            <Outlet/>
+            <Outlet />
           </PopupContext.Provider>
         </section>
       </main>
     </>
   );
-}
- 
+};
+
 export default Navbar;
 export { PopupContext };
