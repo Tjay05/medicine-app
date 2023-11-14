ee5e4927798203533d13a9cfda741986ee7bb8ae
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-11-14 23:21:14 +0100

    format document

--- src/pages/animalMeds/AnimalWrap.jsx
+++ src/pages/animalMeds/AnimalWrap.jsx
@@ -1,45 +1,49 @@
-import { Outlet, NavLink, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, NavLink, useLocation, useNavigate } from "react-router-dom";
 
 // Pic Imports
-import search from '../../assets/icons/search.svg';
-import notification from '../../assets/icons/notification.svg';
-import Popup from '../Popup/Popup';
+import search from "../../assets/icons/search.svg";
+import notification from "../../assets/icons/notification.svg";
+import Popup from "../Popup/Popup";
 
 const AnimalWrap = () => {
   const location = useLocation();
   const history = useNavigate();
 
-  const button = location.pathname.includes('/Expired') || location.pathname.includes('/Items-Sold');
+  const button =
+    location.pathname.includes("/Expired") ||
+    location.pathname.includes("/Items-Sold");
 
-  return ( 
+  return (
     <>
       <header>
         <div className="headerWrap">
           <nav className="topHeader">
             <h1>Animal Medicines</h1>
-            <img onClick={() => history('../Notifications')} src={notification} alt="Bell Icon" />
+            <img
+              onClick={() => history("../Notifications")}
+              src={notification}
+              alt="Bell Icon"
+            />
           </nav>
           <div className="bottomHeader tree">
             <div className="searchForm">
               <img src={search} alt="" />
-              <input placeholder='Search' type="text" />
+              <input placeholder="Search" type="text" />
             </div>
-            {!button && (
-              <Popup/>
-            )}
+            {!button && <Popup />}
           </div>
         </div>
       </header>
       <nav className=" wrapper meds">
-        <NavLink to='/Animal-Medicine'>Available Stock</NavLink>
-        <NavLink to='Items-Sold'>Item sold</NavLink>
-        <NavLink to='Expired'>Expired</NavLink>
+        <NavLink to="/Animal-Medicine">Available Stock</NavLink>
+        <NavLink to="Items-Sold">Item sold</NavLink>
+        <NavLink to="Expired">Expired</NavLink>
       </nav>
       <main className="wrapper">
-        <Outlet/>
+        <Outlet />
       </main>
     </>
   );
-}
- 
+};
+
 export default AnimalWrap;
