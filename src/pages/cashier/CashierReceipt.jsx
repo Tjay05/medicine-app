80facbbbd02ced79d5b7e421f1a3b82b1575d27d
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-11-23 21:30:32 +0100

    format document

--- src/pages/cashier/CashierReceipt.jsx
+++ src/pages/cashier/CashierReceipt.jsx
@@ -1,7 +1,7 @@
 // Pic Imports
-import { useNavigate } from 'react-router-dom';
-import notification from '../../assets/icons/notification.svg'
-import search from '../../assets/icons/search.svg'
+import { useNavigate } from "react-router-dom";
+import notification from "../../assets/icons/notification.svg";
+import search from "../../assets/icons/search.svg";
 
 const CashReciept = () => {
   const history = useNavigate();
@@ -12,12 +12,16 @@ const CashReciept = () => {
         <div className="headerWrap">
           <nav className="topHeader">
             <h1>Receipt</h1>
-            <img onClick={() => history('../Cashier-Notification')} src={notification} alt="Bell Icon" />
+            <img
+              onClick={() => history("../Cashier-Notification")}
+              src={notification}
+              alt="Bell Icon"
+            />
           </nav>
           <div className="bottomHeader tree">
             <div className="searchForm">
               <img src={search} alt="Search" />
-              <input placeholder='Search' type="text" />
+              <input placeholder="Search" type="text" />
             </div>
           </div>
         </div>
@@ -41,11 +45,11 @@ const CashReciept = () => {
             <p>10</p>
             <p>N1000</p>
             <p>Stella</p>
-          </div>    
+          </div>
         </div>
       </main>
     </>
   );
-}
- 
+};
+
 export default CashReciept;
