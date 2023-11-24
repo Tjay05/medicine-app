f4e093283b697fe0a9bbda8c35c89d224d85bb82
Olatayo Tijesu <tjayolatayo@gmail.com>
2023-11-24 23:02:43 +0100

    format document

--- src/pages/cashier/Notifications.jsx
+++ src/pages/cashier/Notifications.jsx
@@ -1,7 +1,7 @@
 // Pic Imports
-import notification from '../../assets/icons/notification.svg'
-import search from '../../assets/icons/search.svg'
-import exclaim from '../../assets/icons/exclame.svg'
+import notification from "../../assets/icons/notification.svg";
+import search from "../../assets/icons/search.svg";
+import exclaim from "../../assets/icons/exclame.svg";
 
 const CashNotifications = () => {
   return (
@@ -15,19 +15,22 @@ const CashNotifications = () => {
           <div className="bottomHeader tree">
             <div className="searchForm">
               <img src={search} alt="Search" />
-              <input placeholder='Search' type="text" />
+              <input placeholder="Search" type="text" />
             </div>
           </div>
         </div>
       </header>
       <main className="notification wrapper">
         <div className="notification-preview">
-          <p className="notification-message">Nise syrup is expiring soon. Ensure timely replacement to maintain the safety of your patients.</p>
+          <p className="notification-message">
+            Nise syrup is expiring soon. Ensure timely replacement to maintain
+            the safety of your patients.
+          </p>
           <img src={exclaim} alt="" />
         </div>
       </main>
     </>
   );
-}
- 
+};
+
 export default CashNotifications;
