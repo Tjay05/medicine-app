dc09733005e1a1c8c0123af3f48fc3b76e178fd2
Olatayo Tijesu <tjayolatayo@gmail.com>
2024-01-05 23:51:50 +0100

    Format document

--- src/pages/Popup/PopupFields/Packs.jsx
+++ src/pages/Popup/PopupFields/Packs.jsx
@@ -1,73 +1,106 @@
 import { useContext } from "react";
 import { PopupContext } from "../../../layout/Navbar";
 
-const Packs = (
-  {
-    Reminder, setReminder, pack, setPack, card, setCard, tablet, setTablet, Total_pieces, setTotal_pieces, weight, setWeight, price, setPrice, expiryDate, setExpiryDate
-  }
-) => {
+const Packs = ({
+  Reminder,
+  setReminder,
+  pack,
+  setPack,
+  card,
+  setCard,
+  tablet,
+  setTablet,
+  Total_pieces,
+  setTotal_pieces,
+  weight,
+  setWeight,
+  price,
+  setPrice,
+  expiryDate,
+  setExpiryDate,
+}) => {
   const { quantity_type, handleTypeChange } = useContext(PopupContext);
 
-  return ( 
+  return (
     <>
       <div className="firstForm">
-        <label htmlFor="type">Quantity Type</label><br />
+        <label htmlFor="type">Quantity Type</label>
+        <br />
         <select value={quantity_type} onChange={handleTypeChange} id="type">
           <option value={quantity_type}>Pack</option>
           <option value="carton">Carton</option>
           <option value="card">Cards</option>
           <option value="tablet">Tablets</option>
         </select>
-        <label htmlFor="pack">Quantity of packs</label><br />
-        <input 
-          type="number" id='pack' 
+        <label htmlFor="pack">Quantity of packs</label>
+        <br />
+        <input
+          type="number"
+          id="pack"
           value={pack}
           onChange={(e) => setPack(Number(e.target.value))}
-        /><br />
-        <label htmlFor="pertablets">Num. of tablets per card</label><br />
-        <input 
-          type="number" id='pertablets' 
+        />
+        <br />
+        <label htmlFor="pertablets">Num. of tablets per card</label>
+        <br />
+        <input
+          type="number"
+          id="pertablets"
           value={tablet}
           onChange={(e) => setTablet(Number(e.target.value))}
-        /><br />
-        <label htmlFor="weight">Net weight (gm/ml)s</label><br />
-        <input 
-          type="text" id='weight' 
+        />
+        <br />
+        <label htmlFor="weight">Net weight (gm/ml)s</label>
+        <br />
+        <input
+          type="text"
+          id="weight"
           value={weight}
           onChange={(e) => setWeight(e.target.value)}
-        /><br />
+        />
+        <br />
       </div>
       <div className="secondForm">
-        <label htmlFor="reminder">Set Timer (Reminder)</label><br />
-        <input 
-          type="text" id='reminder' 
+        <label htmlFor="reminder">Set Timer (Reminder)</label>
+        <br />
+        <input
+          type="text"
+          id="reminder"
           value={Reminder}
           onChange={(e) => setReminder(e.target.value)}
-        /><br />
+        />
+        <br />
         {/* <select id="reminder">
           <option value="">Remind on Tue</option>
         </select> */}
-        <label htmlFor="perpieces">Num. of Cards per pack</label><br />
-        <input 
-          type="number" id='percarton' 
+        <label htmlFor="perpieces">Num. of Cards per pack</label>
+        <br />
+        <input
+          type="number"
+          id="percarton"
           value={card}
           onChange={(e) => setCard(e.target.value)}
-        /><br />
-        <label htmlFor="date">Expiry Date</label><br />
-        <input 
-          type="month" id="date" 
+        />
+        <br />
+        <label htmlFor="date">Expiry Date</label>
+        <br />
+        <input
+          type="month"
+          id="date"
           value={expiryDate}
           onChange={(e) => setExpiryDate(e.target.value)}
         />
-        <label htmlFor="price">Price NGN</label><br />
-        <input 
-          type="number" id="price" 
+        <label htmlFor="price">Price NGN</label>
+        <br />
+        <input
+          type="number"
+          id="price"
           value={price}
           onChange={(e) => setPrice(Number(e.target.value))}
         />
       </div>
     </>
   );
-}
- 
+};
+
 export default Packs;
